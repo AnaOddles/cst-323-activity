c5b43c08be76ce021b32ff0e0d6be2577a82eafd
melzs0627 <melzs0627@gmail.com>
2021-09-28 15:43:35 -0700

    Add Routing Between Pages
    Added routing on nav bar
    Added skeleton controllers and views

---
+++ milestone/src/main/java/com/gcu/controller/RegisterController.java
@@ -0,0 +1,24 @@
+package com.gcu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+	
+	@GetMapping("")
+	public String display(Model model) {
+		model.addAttribute("title", "Register");
+		return "register";
+	}
+	
+	@PostMapping("/doRegister")
+	public String doRegister() {
+		return "registerSuccess";
+	}
+	
+}

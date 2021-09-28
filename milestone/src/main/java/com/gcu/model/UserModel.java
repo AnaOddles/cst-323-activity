cce773d2483f4795c99a535c1dc15e7f2f2e8e94
melzs0627 <melzs0627@gmail.com>
2021-09-28 16:03:52 -0700

    Update UserModel Validation
    Update with Empty and Blank validation annotations

--- milestone/src/main/java/com/gcu/model/UserModel.java
+++ milestone/src/main/java/com/gcu/model/UserModel.java
@@ -3,17 +3,23 @@ package com.gcu.model;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class UserModel {
 
 	private long id;
-
+	
+	@NotEmpty(message = "First Name cannot be empty")
+	@NotBlank(message = "First Name cannot be blank")
 	@NotNull(message = "First Name is a required field")
 	@Size(min = 1, max = 20, message = "First Name must be between 1 and 20 characters")
 	private String firstName;
-
+	
+	@NotEmpty(message = "Last Name cannot be empty")
+	@NotBlank(message = "Last Name cannot be blank")
 	@NotNull(message = "Last Name is a required field")
 	@Size(min = 1, max = 20, message = "Last Name must be between 1 and 20 characters")
 	private String lastName;
@@ -21,14 +27,20 @@ public class UserModel {
 	@Min(value = 5, message = "Age cannot be less than 5")
 	@Max(value = 150, message = "Age cannot be greater than 150")
 	private int age;
-
+	
+	@NotEmpty(message = "Email cannot be empty")
+	@NotBlank(message = "Email cannot be blank")
 	@Email(message="Please enter a valid email address")
 	private String email;
 	
+	@NotEmpty(message = "Username cannot be empty")
+	@NotBlank(message = "Username cannot be blank")
 	@NotNull(message = "Username is a required field")
 	@Size(min = 1, max = 20, message = "Username must be between 1 and 20 characters")
 	private String username;
 	
+	@NotEmpty(message = "Password cannot be empty")
+	@NotBlank(message = "Password cannot be blank")
 	@NotNull(message = "Password is a required field")
 	@Size(min = 1, max = 30, message = "Password must be between 1 and 30 characters")
 	private String password;

984086721b51c1db7b645a1361c486b62c90bb50
melzs0627 <melzs0627@gmail.com>
2021-09-28 13:34:43 -0700

    Initial Commit

---
+++ milestone/src/main/java/com/gcu/Milestone.java
@@ -0,0 +1,13 @@
+package com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Milestone {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Milestone.class, args);
+	}
+
+}

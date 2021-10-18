a8ec24c356a799d599087539490765d80be0deb1
Ana Sanchez <48583891+AnaOddles@users.noreply.github.com>
2021-10-18 00:33:09 -0700

    Getting setup to implement spring beans and Spring Core

---
+++ milestone/src/main/java/com/gcu/ServiceConfig.java
@@ -0,0 +1,14 @@
+package com.gcu;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Class that is used to declare our spring beans
+ * @author anasanchez
+ *
+ */
+@Configuration
+public class ServiceConfig {
+
+	
+	
+}

ff544d3ecb9c3295555834602fee11c307c74166
melzs0627 <melzs0627@gmail.com>
2021-11-02 19:32:49 -0700

    Create Product Entity

--- milestone/src/main/java/com/gcu/data/entity/ProductEntity.java
+++ milestone/src/main/java/com/gcu/data/entity/ProductEntity.java
@@ -1,5 +1,120 @@
 package com.gcu.data.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.MappedCollection;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("games")
 public class ProductEntity {
+
+	@Id
+	Long id;
+
+	@MappedCollection(idColumn = "userId")
+	Long userId;
+
+	@Column("name")
+	String name;
+
+	@Column("publisher")
+	String publisher;
+
+	@Column("genre")
+	String genre;
+
+	@Column("rating")
+	String rating;
+
+	@Column("releaseDate")
+	String releaseDate;
+
+	@Column("image")
+	String image;
+
+	@Column("description")
+	String description;
+
+	public ProductEntity(Long id, String name, String publisher, String genre, String rating, String releaseDate,
+			String image, String description) {
+		this.id = id;
+		this.name = name;
+		this.publisher = publisher;
+		this.genre = genre;
+		this.rating = rating;
+		this.releaseDate = releaseDate;
+		this.image = image;
+		this.description = description;
+	}
+
+	public ProductEntity() {
+
+	}
+
+	// GETTERS AND SETTERS
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 }

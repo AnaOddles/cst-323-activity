ddbfaa98ac36cd3840035036b9e46d8879cef4d3
melzs0627 <melzs0627@gmail.com>
2021-11-04 10:06:01 -0700

    Update ProductsRepository.java

--- milestone/src/main/java/com/gcu/data/repository/ProductsRepository.java
+++ milestone/src/main/java/com/gcu/data/repository/ProductsRepository.java
@@ -1,7 +1,5 @@
 package com.gcu.data.repository;
 
-import java.util.List;
-
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 

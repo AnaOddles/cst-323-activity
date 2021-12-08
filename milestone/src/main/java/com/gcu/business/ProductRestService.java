4c35865ca741b0c476b32b645a112835864bd7a4
melzs0627 <melzs0627@gmail.com>
2021-12-07 21:16:39 -0700

    Update ProductRestService.java

--- milestone/src/main/java/com/gcu/business/ProductRestService.java
+++ milestone/src/main/java/com/gcu/business/ProductRestService.java
@@ -3,7 +3,9 @@ package com.gcu.business;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +32,16 @@ public class ProductRestService {
 	}
 
 	@GetMapping(path = "/getproduct/{id}")
-	public ProductModel getProductById(@PathVariable("id") long id) throws DatabaseException {
-		return service.getByGameId(id);
+	public ResponseEntity<?> getProductById(@PathVariable("id") long id) throws DatabaseException {
+		try {
+			ProductModel product = service.getByGameId(id);
+			if (product == null) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			} else {
+				return new ResponseEntity<>(product, HttpStatus.OK);
+			} 
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }

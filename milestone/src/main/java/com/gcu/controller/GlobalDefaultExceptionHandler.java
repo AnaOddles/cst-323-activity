7b12763b7c417ee44ec1ff30aacbdcc5331db5ab
melzs0627 <melzs0627@gmail.com>
2021-12-13 21:40:59 -0700

    com.gcu.controller

--- milestone/src/main/java/com/gcu/controller/GlobalDefaultExceptionHandler.java
+++ milestone/src/main/java/com/gcu/controller/GlobalDefaultExceptionHandler.java
@@ -1,7 +1,7 @@
 // Melanie Spence and Ana Sanchez
 // CST-339
 // Milestone
-// November 7, 2021
+// December 13, 2021
 // This is our own work
 
 // Source: https://github.com/markreha/myprivatecloud
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+// Annotation for ControllerAdvice
 @ControllerAdvice
 public class GlobalDefaultExceptionHandler
 {

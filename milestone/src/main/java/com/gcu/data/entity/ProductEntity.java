c68d0e96a8fdaa2c720a1cc41ca224d53bf3bbd8
melzs0627 <melzs0627@gmail.com>
2021-12-13 21:46:24 -0700

    com.gcu.data.entity

--- milestone/src/main/java/com/gcu/data/entity/ProductEntity.java
+++ milestone/src/main/java/com/gcu/data/entity/ProductEntity.java
@@ -1,7 +1,7 @@
 // Melanie Spence and Ana Sanchez
 // CST-339
 // Milestone
-// November 7, 2021
+// December 13, 2021
 // This is our own work
 
 package com.gcu.data.entity;
@@ -16,6 +16,7 @@ import org.springframework.data.relational.core.mapping.Table;
  * @author melzs
  *
  */
+// Table for entities
 @Table("games")
 public class ProductEntity {
 
